a410c2d6f92cb73acf8775174e08d47021463b96
Md Rijoan Maruf <rmsaif4430@gmail.com>
2025-03-01 02:21:04 +0600

    Added History

---
+++ JS/clearHistory.js
@@ -0,0 +1,10 @@
+document.getElementById('clearHistory').addEventListener('click' , function(){
+    document.getElementById('history').innerHTML = ''
+})
+// document.getElementById('clearHistory').addEventListener('click' , function(){
+//     document.getElementById('history').innerHTML = `
+//     <p class="text-center mt-4 font-semibold text-green-300">
+//         Cleared All History.
+//     </p>
+//     `
+// })
